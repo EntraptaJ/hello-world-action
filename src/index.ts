59a89d023d326cc6c325309e5382843e68cd33c2
Kristian F Jones <me@kristianjones.xyz>
2019-09-13 22:28:11 -0500

    hjkhj

--- src/index.ts
+++ src/index.ts
@@ -2,14 +2,16 @@
 import { setOutput, getInput } from '@actions/core';
 import { resolve } from 'path';
 import { sync } from 'glob';
+import { readdir } from 'fs-extra'
 import run from './run'
 
 async function helloWorld(): Promise<void> {
   const filenames = sync(`${process.env.GITHUB_WORKSPACE}/**/*`);
-  console.log(filenames)
-  for (const filename of filenames) {
-    console.log(filename);
-  }
+  readdir(process.env.GITHUB_WORKSPACE, (err, files) => {
+    files.forEach(file => {
+      console.log(file);
+    });
+  });
 
   const input1 = getInput('input-1');
   console.log(`Input #1 is set to ${input1}`);
