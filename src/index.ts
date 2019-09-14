e639b04b3f0db363f129a9dbc26ddbd7fae0a088
Kristian F Jones <me@kristianjones.xyz>
2019-09-13 22:13:03 -0500

    feat: Stuff

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,16 @@
 // src/index.ts
 import { setOutput, getInput } from '@actions/core';
+import glob from 'glob'
+import { resolve } from 'path'
 
 async function helloWorld(): Promise<void> {
     const input1 = getInput('input-1'); 
     console.log(`Input #1 is set to ${input1}`)
+}
+
+const filenames = glob.sync(resolve(`${process.env.GITHUB_WORKSPACE}`))
+for (const filename of filenames) {
+  console.log(filename)
 }
 
 helloWorld();
