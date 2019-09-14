77872ccdd05280f16d6af23a9d555686afe71ffb
Kristian F Jones <me@kristianjones.xyz>
2019-09-13 22:34:48 -0500

    feat: Run tests

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,17 @@
 // src/index.ts
 import { setOutput, getInput } from '@actions/core';
-import { resolve } from 'path';
+import { resolve, parse } from 'path';
 import { sync } from 'glob';
 import { readdir } from 'fs-extra'
 import run from './run'
 
 async function helloWorld(): Promise<void> {
-  const filenames = sync(`${process.env.GITHUB_WORKSPACE}/**/*`);
-  readdir(process.env.GITHUB_WORKSPACE, (err, files) => {
-    files.forEach(file => {
-      console.log(file);
-    });
-  });
+  const filenames = sync(`${process.env.GITHUB_WORKSPACE}/**/package.json`);
+  
+  for (const filename of filenames) {
+    await run(`npm install`, { cwd: parse(filename).dir })
+    await run(`npm test`)
+  }
 
   const input1 = getInput('input-1');
   console.log(`Input #1 is set to ${input1}`);
