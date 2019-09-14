8d088463010a11ae949222384d25fe0ee6b98951
Kristian F Jones <me@kristianjones.xyz>
2019-09-13 22:18:40 -0500

    feat: Stuff

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,16 @@
 // src/index.ts
 import { setOutput, getInput } from '@actions/core';
-import glob from 'glob'
-import { resolve } from 'path'
+import { resolve } from 'path';
+import { sync } from 'glob';
 
 async function helloWorld(): Promise<void> {
-    const input1 = getInput('input-1'); 
-    console.log(`Input #1 is set to ${input1}`)
-}
+  const filenames = sync(`${process.env.GITHUB_WORKSPACE}`);
+  for (const filename of filenames) {
+    console.log(filename);
+  }
 
-const filenames = glob.sync(resolve(`${process.env.GITHUB_WORKSPACE}`))
-for (const filename of filenames) {
-  console.log(filename)
+  const input1 = getInput('input-1');
+  console.log(`Input #1 is set to ${input1}`);
 }
 
 helloWorld();
