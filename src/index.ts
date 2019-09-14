df5befd545c0732df82483403dbdb0c0dc62bfaa
Kristian F Jones <me@kristianjones.xyz>
2019-09-13 22:38:05 -0500

    feat: Failed tests

--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,28 @@
 // src/index.ts
-import { setOutput, getInput } from '@actions/core';
+import { setOutput, getInput, setFailed } from '@actions/core';
 import { resolve, parse } from 'path';
 import { sync } from 'glob';
-import { readdir } from 'fs-extra'
-import run from './run'
+import { readdir } from 'fs-extra';
+import run from './run';
 
 async function helloWorld(): Promise<void> {
-  const filenames = sync(`${process.env.GITHUB_WORKSPACE}/**/package.json`);
-  
-  for (const filename of filenames) {
-    await run(`npm install`, { cwd: parse(filename).dir })
-    await run(`npm test`)
-  }
+  try {
+    const filenames = sync(`${process.env.GITHUB_WORKSPACE}/**/package.json`);
 
-  const input1 = getInput('input-1');
-  console.log(`Input #1 is set to ${input1}`);
+    for (const filename of filenames) {
+      await run(`npm install`, { cwd: parse(filename).dir });
+      try {
+        await run(`npm test`);
+      } catch {
+        setFailed('TEST FAILED');
+      }
+    }
+
+    const input1 = getInput('input-1');
+    console.log(`Input #1 is set to ${input1}`);
+  } catch (error) {
+    setFailed(error.message);
+  }
 }
 
 helloWorld();
