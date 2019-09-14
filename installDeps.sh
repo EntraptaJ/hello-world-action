ef20b4ca7c8364a60974c780931248b204361257
Kristian F Jones <me@kristianjones.xyz>
2019-09-13 22:05:29 -0500

    feat: Use Parcel to bundle instead of including node_modules

--- installDeps.sh
+++
@@ -1,9 +0,0 @@
-export NODE_ENV=development 
-npm ci
-npm run build
-rm -rf ./node_modules
-export NODE_ENV=production 
-npm ci --prod
-git add node_modules
-git commit -m "Add prod node modules"
-git push -u origin master
--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,9 @@
 // src/index.ts
-import * as core from '@actions/core';
+import { setOutput, getInput } from '@actions/core';
 
 async function helloWorld(): Promise<void> {
-  try {
-    const input1 = core.getInput('input-1');
+    const input1 = getInput('input-1'); 
     console.log(`Input #1 is set to ${input1}`)
-    const time = new Date().toTimeString();
-    core.setOutput('time', time);
-  } catch (error) {
-    core.setFailed(error.message);
-  }
 }
 
 helloWorld();
